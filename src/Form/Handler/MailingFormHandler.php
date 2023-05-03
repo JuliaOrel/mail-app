4cd4eb4a3fe900f1393f10ee8877ac9315bec62b
Vitaliy <vitosgeen@yandex.ru>
2023-05-03 14:42:13 +0300

    fix processSendMailing

--- src/Form/Handler/MailingFormHandler.php
+++ src/Form/Handler/MailingFormHandler.php
@@ -83,9 +83,11 @@ class MailingFormHandler
         $template = $this->twig->createTemplate($emailBody);
         $emailBody = $template->render($data);
         $emailText = strip_tags($emailBody);
+        $fname = (!empty($data["user_name"]))?$data["user_name"]:"";
+        $lname = (!empty($data["user_surname"]))?$data["user_surname"]:"";
         $sign = $this->_mailingManager->createPixelUserSign($data["user_id"], $mailing->getId());
         $this->editHrefForTrackingFromStr($emailBody, $sign, self::HREF_REDIRECT);
-        $r = $this->mailingEmailSender->senEmail($data["user_email"], $data["user_name"], $data["user_surname"], $emailSubject, $emailBody, $data["unsubsribe_url"]);
+        $r = $this->mailingEmailSender->senEmail($data["user_email"], $fname, $lname, $emailSubject, $emailBody, $data["unsubsribe_url"]);
         $this->_mailingManager->createMailingItem($data["user_id"], $data["user_email"], $mailing->getId());
         if ($r["error"]) {
             $this->_mailingManager->saveMailingItemSendError($data["user_id"], $mailing->getId(), $r["error_text"]);
